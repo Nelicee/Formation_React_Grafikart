5d9b0f82fb124d0af6e990aaaf551bf342da65e9
Céline DEVOS <140008750+Nelicee@users.noreply.github.com>
2024-09-06 11:03:40 +0200

    TP liste de produits

--- React_TP_Liste_Des_Produits/src/App.jsx
+++ React_TP_Liste_Des_Produits/src/App.jsx
@@ -1,29 +1,92 @@
+import { useState } from "react";
+import { Checkbox } from "./components/forms/Checkbox";
+import { Input } from "./components/forms/input";
 
-const todos = [
-  'Présenter react',
-  'Présenter le JSX',
-  'Créer des composants'
-]
+import { ProductCategoryRow } from "./components/products/ProductCategoryRow";
+import { ProductRow } from "./components/products/ProductRow";
+
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
 function App() {
+  const [showStockedOnly, setShowStockedOnly] = useState(false);
+  const [search, setSearch] = useState('');
+
+  const visibleProducts = PRODUCTS.filter((product) => {
+    if (showStockedOnly && !product.stocked) {
+      return false;
+    }
+    if (search && !product.name.includes(search)) {
+      return false;
+    }
+    return true;
+  });
   return (
-    <>
-    <Title color="blue" id="monid" className="maclass" data-demo="demo"  >Mon composant</Title>
-      <input type="text" />
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam molestias sequi dolores, officia ipsa iure debitis porro ut cum maiores architecto odit blanditiis impedit obcaecati doloribus, sed alias repellendus.</p>
-      <ul>
-        {todos.map(todo => (<li style={{listStyle: 'none'}} key ={todo}>{todo}</li>))}
-      </ul>
-    </>
+    <div className="container my-3">
+      <SearchBar
+        search={search}
+        onSearchChange={setSearch}
+        showStockedOnly={showStockedOnly}
+        onStockedOnlyChange={setShowStockedOnly}
+      />
+      <ProductTable products={visibleProducts} />
+    </div>
+  );
+}
+function SearchBar({
+  showStockedOnly,
+  onStockedOnlyChange,
+  search,
+  onSearchChange}
+) {
+  return (
+    <div>
+      <div className="mb-3">
+        <Input
+          value={search}
+          onChange={onSearchChange}
+          placeholder="Rechercher..."
+        />
+        <Checkbox
+          id="stocked"
+          checked={showStockedOnly}
+         onChange={onStockedOnlyChange}
+          label="N'afficher que les produits en stock"
+        ></Checkbox>
+      </div>
+    </div>
   );
 }
 
-function Title({color, hidden, ...props}) {
-  if(hidden){
-    return null
+function ProductTable({ products }) {
+  const rows = [];
+  let lastCategory = null;
+
+  for (let product of products) {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow key={product.category} name={product.category} />
+      );
+    }
+    lastCategory = product.category;
+    rows.push(<ProductRow product={product} key={product.name} />);
   }
 
-  
-  return <h1 style={{color: color}} {...props}/>
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Prix</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
 }
-
 export default App;
