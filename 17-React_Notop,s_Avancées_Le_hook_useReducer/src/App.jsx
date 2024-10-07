f96cc9ca3485e31299b13db913fb656aa9c82030
Céline DEVOS <140008750+Nelicee@users.noreply.github.com>
2024-10-07 08:03:59 +0200

    Le hook use Reducer

---
+++ 17-React_Notop,s_Avancées_Le_hook_useReducer/src/App.jsx
@@ -0,0 +1,42 @@
+import {useTodos} from "./hooks/useTodos.js"
+
+
+function App() {
+
+  const {visibleTodos, removeTodo, clearCompleted, toggleFilter, toggleTodo, showCompleted} = useTodos()
+  
+  return (
+    <div>
+      <p>
+        <input
+          type="checkbox"
+          checked={showCompleted}
+          onChange={toggleFilter}
+        />
+        Afficher les tâches accomplies
+      </p>
+      <ul>
+        {visibleTodos.map((todo) => (
+          <li key={todo.name}>
+            <input
+              type="checkbox"
+              onChange={() => toggleTodo(todo)}
+              checked={todo.checked}
+            />
+            {todo.name}
+            <button
+              onClick={() => removeTodo(todo) }
+            >
+              Supprimer
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={clearCompleted}>
+        Supprimer les tâches accomplies
+      </button>
+    </div>
+  );
+}
+
+export default App;
